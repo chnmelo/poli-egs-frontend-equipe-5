79bb2865015a1c3b2295475f79cc0d8607f97cb1
Juliana Danzi <julianadanzi3@gmail.com>
2025-06-02 19:56:50 -0300

    feat(OT2-80): lista de semestre para projetos e artigos para admin e user-comum

--- src/pages/Admin/Artigos.tsx
+++ src/pages/Admin/Artigos.tsx
@@ -172,6 +172,22 @@ function ArticlesAdmin () {
       article.tema?.toLowerCase().includes(input)
     );
   }) : [];
+  const semesterGenerator = (): string[] => {
+    const current = new Date();
+    const currentYear = current.getFullYear();
+    const currentMonth = current.getMonth();
+
+    const semesters: string[] = [];
+
+    for (let year = 2023; year <= currentYear; year++) {
+      semesters.push(`${year}.1`);
+      if (year < currentYear || currentMonth >= 6) {
+        semesters.push(`${year}.2`);
+      }
+    }
+
+    return semesters.reverse();
+    };
 
   return (
     <>
@@ -377,14 +393,17 @@ function ArticlesAdmin () {
                     />
                   </div>
                   <div>
-                    <h3 className="text-lg font-semibold">Data de publicação</h3>
-                    <input
-                      id="datapublicacao"
-                      type="date"
-                      name="datapublicacao"
+                    <h3 className="text-lg font-semibold">Semestre</h3>
+                    <select
+                      name="semestre"
+                      id="semestre"
+                      value={NewArticle.data}
                       className="focus:outline-none border-b-2 w-[15vw]"
-                      onChange={(e) => setNewArticle({ ...NewArticle, data: e.target.value })}
-                    />
+                      onChange={(e) => setNewArticle({ ...NewArticle, data: e.target.value })}>
+                      <option value="">Selecione um semestre</option>
+                      {semesterGenerator().map((semestre) => (
+                          <option key={semestre} value={semestre}>{semestre}</option>))}
+                    </select>
                   </div>
                   <div className="w-[15vw] relative">
                     <input
