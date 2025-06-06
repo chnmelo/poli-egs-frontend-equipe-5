1fbe2b1cded598441b2f3106af48e42a63fd2593
iianTP <158515401+iianTP@users.noreply.github.com>
2025-06-06 10:24:08 -0300

    fix(OT2-73): adaptação dos card do menu para a nova forma de filtragem

--- src/App.tsx
+++ src/App.tsx
@@ -168,7 +168,7 @@ function App() {
           ].map((theme, index) => (
             <button
               key={index}
-              onClick={() => handleButtonClick(theme.key_words)}
+              onClick={() => handleButtonClick({title: theme.title, key_words: theme.key_words})}
               className="relative group overflow-hidden rounded-xl shadow-lg transform hover:scale-105 transition-transform"
             >
               <img
