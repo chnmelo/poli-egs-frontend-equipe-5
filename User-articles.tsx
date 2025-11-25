a4e235cedeb55a89450438570bed58e51f35f239
gu3des446 <jcg.446@gmail.com>
2025-11-24 22:24:32 -0300

    navbar

--- User-articles.tsx
+++ User-articles.tsx
@@ -1,5 +1,5 @@
 import { Table } from "react-bootstrap";
-import HeaderUser from "../../components/HeaderUser";
+import Navbar from "./src/components/Navbar";
 import { SetStateAction, useEffect, useState } from "react";
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from "@headlessui/react";
 import ModalDeleteArticle from "../../components/ModalDeleteArticle";
@@ -241,7 +241,7 @@ function Userarticles () {
 
   return (
     <>
-      <HeaderUser />
+      <Navbar userRole="user" />
       <ToastContainer
         position="top-center"
         autoClose={3000}
